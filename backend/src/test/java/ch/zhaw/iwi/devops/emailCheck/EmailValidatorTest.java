a1c8a0915c029c88f3a93f2d62359ec115ef120b
Pascal Aigner <pascal.aigner97@outlook.com>
2024-05-22 19:25:25 +0200

    Erster Test noch ohne zugehörige Funktion

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/emailCheck/EmailValidatorTest.java
@@ -0,0 +1,13 @@
+package ch.zhaw.iwi.devops.emailvalidator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EmailValidatorTest {
+
+    @Test
+    public void testEmailContainingAtSymbol() {
+        EmailValidator emailValidator = new EmailValidator();
+        Assertions.assertTrue(emailValidator.isValid("test@example.com"));
+    }
+}
