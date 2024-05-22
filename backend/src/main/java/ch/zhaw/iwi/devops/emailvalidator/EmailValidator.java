aedbca687688b30da05edb9079070c368c0ba560
Pascal Aigner <pascal.aigner97@outlook.com>
2024-05-22 20:19:09 +0200

    Funktion erweitert und zweiten Test erneut ausgeführt --> pass

--- backend/src/main/java/ch/zhaw/iwi/devops/emailvalidator/EmailValidator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/emailvalidator/EmailValidator.java
@@ -3,6 +3,9 @@ package ch.zhaw.iwi.devops.emailvalidator;
 public class EmailValidator {
 
     public boolean isValid(String email) {
+        if (email == null) {
+            return false;
+        }
         return email.contains("@");
     }
 }
