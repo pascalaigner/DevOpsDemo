3789bfd5764e6305d991ae1bcb626b75f4213ee6
Pascal Aigner <pascal.aigner97@outlook.com>
2024-05-22 22:51:32 +0200

    Code smell optimiert

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/MathLibTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/MathLibTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class MathLibTest {
+class MathLibTest {
     @Test
     void testIsEven() {
         assertTrue(MathLib.isEven(2));
